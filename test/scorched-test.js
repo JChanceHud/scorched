fdb1adace028eaacbb451bcfd350a79f3513691d
Chance Hudson <jchancehud@gmail.com>
2021-10-04 10:33:50 -0500

    chore: test rate negotiation

--- test/scorched-test.js
+++ test/scorched-test.js
@@ -43,15 +43,20 @@ function encodeAppData(data) {
 function createOutcome(contracts, wallets, balances) {
   const { adjudicator } = contracts
   const { asker, suggester, beneficiary } = wallets
-  const weiBalances = {
-    [convertAddressToBytes32(asker.address)]: ethers.utils.parseEther(balances.asker.toString()),
-    [convertAddressToBytes32(suggester.address)]: ethers.utils.parseEther(balances.suggester.toString()),
-    [convertAddressToBytes32(beneficiary.address)]: ethers.utils.parseEther(balances.beneficiary.toString()),
-  }
-  const allocation = []
-  for (const key of Object.keys(weiBalances)) {
-    allocation.push({ destination: key, amount: weiBalances[key] })
-  }
+  const allocation = [
+    {
+      destination: convertAddressToBytes32(asker.address),
+      amount: balances.asker.toString(),
+    },
+    {
+      destination: convertAddressToBytes32(suggester.address),
+      amount: balances.suggester.toString(),
+    },
+    {
+      destination: convertAddressToBytes32(beneficiary.address),
+      amount: balances.beneficiary.toString(),
+    },
+  ]
   // console.log(Object.keys(wallets).map(w => `${w}: ${wallets[w].address}`))
   // console.log(allocation)
   return [
@@ -135,7 +140,7 @@ describe('Scorched', function () {
 
     // Run deposits
     {
-      const depositAmount = ethers.utils.parseEther('10')
+      const depositAmount = '100000000'
 
       const suggesterDepositTx = await adjudicator.connect(suggester).deposit(
         '0x0000000000000000000000000000000000000000',
@@ -249,7 +254,7 @@ describe('Scorched', function () {
     const { scorched, adjudicator } = await getDeployedContracts()
     const [ sender, asker, suggester, beneficiary ] = await ethers.getSigners()
 
-    const depositAmount = ethers.utils.parseEther('100')
+    const depositAmount = '100000000'
 
     const wallets = [
       ethers.Wallet.createRandom(),
@@ -373,9 +378,9 @@ describe('Scorched', function () {
           queryStatus: QueryStatus.Accepted,
           responseStatus: ResponseStatus.None,
         }
-        balances.asker -= 3
+        balances.asker -= 6
         balances.suggester -= 3
-        balances.beneficiary += 6
+        balances.beneficiary += 9
       } else {
         // asker is responding
         appData = {
@@ -386,9 +391,9 @@ describe('Scorched', function () {
           queryStatus: QueryStatus.None,
           responseStatus: ResponseStatus.Pay,
         }
-        balances.asker -= 3
-        balances.suggester += 6
-        balances.beneficiary -= 6
+        balances.asker -= 0
+        balances.suggester += 9
+        balances.beneficiary -= 9
       }
       const appDataBytes = encodeAppData(appData)
       const nextState = {
@@ -419,5 +424,192 @@ describe('Scorched', function () {
       [0, 1]
     )
     await checkpointTx.wait()
+  })
+
+  it('should negotiate a new rate', async () => {
+    const { scorched, adjudicator } = await getDeployedContracts()
+    const [ sender, asker, suggester, beneficiary ] = await ethers.getSigners()
+
+    const depositAmount = '100000000'
+
+    const wallets = [
+      ethers.Wallet.createRandom(),
+      ethers.Wallet.createRandom(),
+    ]
+
+    const channel = {
+      chainId: '0x1234',
+      channelNonce: BigNumber.from(0).toHexString(),
+      participants: wallets.map(w => w.address),
+    }
+    const channelId = getChannelId(channel)
+    const startingOutcome = createOutcome(
+      { adjudicator },
+      { asker, suggester, beneficiary, },
+      {
+        asker: depositAmount.toString(),
+        suggester: depositAmount.toString(),
+        beneficiary: 0,
+      }
+    )
+
+    const baseState = {
+      isFinal: false,
+      channel,
+      outcome: startingOutcome,
+      appDefinition: scorched.address,
+      appData: ethers.constants.HashZero,
+      challengeDuration: 1,
+    }
+
+    const state0 = {
+      ...baseState,
+      turnNum: 0,
+    }
+
+    const state1 = {
+      ...baseState,
+      turnNum: 1,
+    }
+
+    const preFundSigs = await signStates([state0, state1], wallets, [0, 1])
+
+    const preFundCheckpointTx = await adjudicator.connect(sender).checkpoint(
+      getFixedPart(state1),
+      state1.turnNum,
+      [getVariablePart(state0), getVariablePart(state1)],
+      0,
+      preFundSigs,
+      [0, 1]
+    )
+    await preFundCheckpointTx.wait()
+
+    // Run deposits
+    {
+
+      const suggesterDepositTx = await adjudicator.connect(suggester).deposit(
+        '0x0000000000000000000000000000000000000000',
+        channelId,
+        0,
+        depositAmount,
+        {
+          value: depositAmount.toString()
+        }
+      )
+      await suggesterDepositTx.wait()
+
+      const askerDepositTx = await adjudicator.connect(asker).deposit(
+        '0x0000000000000000000000000000000000000000',
+        channelId,
+        depositAmount,
+        depositAmount,
+        {
+          value: depositAmount.toString()
+        }
+      )
+      await askerDepositTx.wait()
+    }
+
+    // Contract is funded, post fund checkpoint
+
+    const state2 = {
+      ...baseState,
+      turnNum: 2,
+    }
+
+    const state3 = {
+      ...baseState,
+      turnNum: 3,
+    }
+
+    const postFundSigs = await signStates([state2, state3], wallets, [0, 1])
+
+    const postFundCheckpointTx = await adjudicator.connect(sender).checkpoint(
+      getFixedPart(state3),
+      state3.turnNum,
+      [getVariablePart(state2), getVariablePart(state3)],
+      0,
+      postFundSigs,
+      [0, 1]
+    )
+    await postFundCheckpointTx.wait()
+    const balances = {
+      asker: +depositAmount.toString(),
+      suggester: +depositAmount.toString(),
+      beneficiary: 0,
+    }
+
+    const appData = {
+      payment: BigNumber.from(10).toString(),
+      askerBurn: BigNumber.from(3).toString(),
+      suggesterBurn: BigNumber.from(3).toString(),
+      status: AppStatus.Negotiate,
+      queryStatus: QueryStatus.None,
+      responseStatus: ResponseStatus.None,
+    }
+    const state4 = {
+      ...baseState,
+      turnNum: 4,
+      outcome: createOutcome(
+        { adjudicator, },
+        { asker, suggester, beneficiary },
+        balances,
+      ),
+      appData: encodeAppData(appData),
+    }
+    const state5 = {
+      ...state4,
+      turnNum: 5,
+      appData: encodeAppData({
+        ...appData,
+        payment: BigNumber.from(20).toString(),
+        askerBurn: BigNumber.from(6).toString(),
+        suggesterBurn: BigNumber.from(6).toString(),
+      })
+    }
+    const sig1 = await signState(state4, wallets[0].privateKey)
+    const sig2 = await signState(state5, wallets[1].privateKey)
+    const tx = await adjudicator.connect(sender).checkpoint(
+      getFixedPart(state5),
+      state5.turnNum,
+      [getVariablePart(sig1.state), getVariablePart(sig2.state)],
+      0,
+      [sig1.signature, sig2.signature],
+      [0, 1]
+    )
+    await tx.wait()
+
+    const acceptAppData = {
+      payment: BigNumber.from(20).toString(),
+      askerBurn: BigNumber.from(6).toString(),
+      suggesterBurn: BigNumber.from(6).toString(),
+      status: AppStatus.Answer,
+      queryStatus: QueryStatus.Accepted,
+      responseStatus: ResponseStatus.None,
+    }
+    const state6 = {
+      ...baseState,
+      turnNum: 6,
+      appData: encodeAppData(acceptAppData),
+      outcome: createOutcome(
+        { adjudicator, },
+        { asker, suggester, beneficiary },
+        {
+          asker: BigNumber.from(depositAmount).sub(acceptAppData.payment).toString(),
+          suggester: BigNumber.from(depositAmount).sub(acceptAppData.suggesterBurn).toString(),
+          beneficiary: BigNumber.from(26).toString(),
+        },
+      ),
+    }
+    const acceptSig = await signState(state6, wallets[0].privateKey)
+    const acceptTx = await adjudicator.connect(sender).checkpoint(
+      getFixedPart(state6),
+      state6.turnNum,
+      [getVariablePart(sig2.state), getVariablePart(acceptSig.state)],
+      0,
+      [sig2.signature, acceptSig.signature],
+      [1, 0],
+    )
+    await acceptTx.wait()
   })
 })
