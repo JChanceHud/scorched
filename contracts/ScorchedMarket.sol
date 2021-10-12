f15bc4b1c01811e601b8c6d4167fa65840ad1f03
Chance Hudson <jchancehud@gmail.com>
2021-10-11 19:15:41 -0500

    feat: market bio, helper functions

--- contracts/ScorchedMarket.sol
+++ contracts/ScorchedMarket.sol
@@ -6,17 +6,25 @@ contract ScorchedMarket {
   mapping (address => uint) suggesterIndexes;
   mapping (address => string) public suggesterUrls;
   mapping (address => string) public suggesterNames;
+  mapping (address => string) public suggesterBios;
+
+  // TODO: emit an event when these things change
 
   constructor() {
     suggesters.push(address(0));
   }
 
-  function registerSuggester(string memory url, string memory name) public {
+  function registerSuggester(
+    string memory url,
+    string memory name,
+    string memory bio
+  ) public {
     require(suggesterIndexes[msg.sender] == 0);
     suggesters.push(msg.sender);
     suggesterIndexes[msg.sender] = suggesters.length - 1;
     suggesterUrls[msg.sender] = url;
     suggesterNames[msg.sender] = name;
+    suggesterBios[msg.sender] = bio;
   }
 
   function updateUrl(string memory url) public {
@@ -29,6 +37,11 @@ contract ScorchedMarket {
     suggesterNames[msg.sender] = name;
   }
 
+  function updateBio(string memory bio) public {
+    require(suggesterIndexes[msg.sender] != 0);
+    suggesterBios[msg.sender] = bio;
+  }
+
   // must be sent by suggester
   function unregisterSuggester() public {
     require(suggesterIndexes[msg.sender] != 0);
@@ -37,6 +50,34 @@ contract ScorchedMarket {
     suggesterIndexes[msg.sender] = 0;
     suggesterUrls[msg.sender] = '';
     suggesterNames[msg.sender] = '';
+    suggesterBios[msg.sender] = '';
+  }
+
+  function suggesterInfoByIndex(uint index) public view returns (
+    address,
+    string memory,
+    string memory,
+    string memory
+  ) {
+    address suggester = suggesters[index];
+    return (
+      suggester,
+      suggesterUrls[suggester],
+      suggesterNames[suggester],
+      suggesterBios[suggester]
+    );
+  }
+
+  function suggesterInfo(address suggester) public view returns (
+    string memory,
+    string memory,
+    string memory
+  ) {
+    return (
+      suggesterUrls[suggester],
+      suggesterNames[suggester],
+      suggesterBios[suggester]
+    );
   }
 
   function suggesterCount() public view returns (uint) {
